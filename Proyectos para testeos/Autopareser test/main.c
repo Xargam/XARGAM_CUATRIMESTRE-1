0afbe39ab710557d7d9cb2c2313468a63b84f0bc
Xargam <leoxargam@gmail.com>
2018-06-20 15:36:18 -0300

    Autoparser final

--- Proyectos para testeos/Autopareser test/main.c
+++ Proyectos para testeos/Autopareser test/main.c
@@ -3,9 +3,9 @@
 #include <stdarg.h>
 #include <string.h>
 #include "xlook.h"
-#define ARCH "data.csv"
+#define ARCH "autos.csv"
 
-int csvGenericParser(FILE* filePointer, int dataToParse,char format[], int totalArguments,...);
+int csvGenericParser(FILE* filePointer, int dataToParse,char* format, int totalArguments,...);
 int startStringChecker(char* stringToLook,int totalArguments,...);
 void stringCutter(char* stringToCut, int index );
 void* stringConditionalConversor(char* inputString, char* stringCompare, void* output );
@@ -13,38 +13,38 @@ void* stringConditionalConversor(char* inputString, char* stringCompare, void* o
 typedef struct
 {
     int id;
-    char nombre[200];
-    char apellido[200];
-    int estado;
-} ePersonas;
+    char marca[200];
+    char modelo[200];
+    char color[200];
+    int anio;
+
+} eAutos;
 
 int main()
 {
-    ePersonas personas[2000];
-    char format[]= {"%d;%s;%s;-->%s;%d;%s;%d;<--"};
-    int case1 = 1;
-    int case2 = 0;
-
-    char strCmp[]= {"TRUE"};
-    char strCmp2[]= {"FALSE"};
-
+    eAutos autos[2000];
     FILE* file = fopen(ARCH,"r");
     if( file == NULL)
     {
         file = fopen(ARCH, "w");
     }
     int i = 0;
-    fscanf(file,"%[^\n]\n",personas[i].nombre);
-    printf("LONGI: %ld",ftell(file) );
+    fscanf(file,"%[^\n]\n",autos[i].marca);
     system("pause");
-    while( csvGenericParser(file,4,format,8,&personas[i].id, personas[i].nombre, personas[i].apellido, &personas[i].estado , strCmp , &case1 , strCmp2 , &case2  ))
+    while( csvGenericParser(file,5,"%d;%s;%s;%s;%d;",5, &autos[i].id ,autos[i].marca, autos[i].modelo ,autos[i].color , &autos[i].anio ))
     {
-        xlkSortPrintf(1,"%d,%s,%s,%d",4,personas[i].id , personas[i].nombre , personas[i].apellido, personas[i].estado);
+        xlkSortPrintf(1,"%d,%s,%s,%s,%d",5,autos[i].id , autos[i].marca , autos[i].modelo,autos[i].color , autos[i].anio);
         i++;
     }
     fclose(file);
     return 0;
 }
+
+
+
+
+
+
 
 
 void* stringConditionalConversor(char* inputString, char* stringCompare, void* output )
@@ -57,33 +57,46 @@ void* stringConditionalConversor(char* inputString, char* stringCompare, void* o
     return returnPointer;
 }
 
-int csvGenericParser( FILE* filePointer, int dataToParse,char format[], int totalArguments,...)
+int csvGenericParser( FILE* filePointer, int dataToParse,char* format, int totalArguments,...)
 {
     int verify = 0;
+
     if( dataToParse < 21 && dataToParse > -1 && totalArguments < 201 && totalArguments > -1 && filePointer != NULL && format != NULL && strlen(format) < 1000 && !feof(filePointer) )
     {
-        char auxFormat[1000];
-        strcpy(auxFormat,format);
-        verify = 1;
+        char format2[1000];
+        strcpy(format2 , format);
+        verify = 1; //Variable de retorno indicando error o final de archivo.
         va_list arguments;
         va_start(arguments, totalArguments );
 
-        char matrizDatos[20][1000];
-        char conversionArgument[1000];
-        char argumentToCompare[1000];
+        char matrizDatos[20][1000]; //Matriz que guarda strings con los datos leidos de una linea del archivo que se debe parsear. El parser soporta archivos de 20 datos.
+        char conversionArgument[1000]; //Cuando el usuario quiere convertir un dato leido del archivo en otro string o en otro tipo de dato, se guarda en esta cadena "especial".
+        char argumentToCompare[1000];  //Recibe parametros con los que se debe comparar un dato leido del archivo para transformarlo.
+
+        //Punteros utilizados para guardar parametros:
+
         char* stringPointer;
         int* integerPointer;
         long* longPointer;
         long long* longLongPointer;
         double* doublePointer;
         void* pointer;
-        int i;
-        int j;
-        int flagConversion = 0;
-        int flag2Conversion = 0;
-        int flag3 = 0;
-        int formatReader;
-        int lastFormat;
+
+        //Numero para guardar parametros de posible conversion:
+
+        int integerNumber; //Entero utilizado para leer argumentos que indican comparaciones de un dato del archivo.
+        double doubleNumber;
+        long longNumber;
+        long long longLongNumber;
+
+        int i; // Variable de iteraciones relacionada con los argumentos que se van leyendo.
+        int j; // Variable de iteraciones relacionada con la matriz de datos leidos del archivo.
+
+        int flagConversion = 0; //Bandera que indica que se estan leyendo parametros relacionados a un dato que se debe convertir.
+        int flag2Conversion = 0; //Bandera que dice al programa si se esta leyendo un string a comparar o un dato que que se podria asignar a un parametro de conversion.
+        int flag3 = 0; // Si se convirtio un parametro esta bandera indica que se detengan las comparaciones con los sigguientes parametros y que se dejen pasar.
+        int formatReader; //Lee la cadena de formato.
+        int lastFormat; // Guarda el ultimo dato de format reader.
 
         switch(dataToParse)
         {
@@ -152,26 +165,25 @@ int csvGenericParser( FILE* filePointer, int dataToParse,char format[], int tota
         {
             if( flagConversion )
             {
-                flag2Conversion = 1 - flag2Conversion;
+                flag2Conversion = 1 - flag2Conversion; // si flag2 es 1 se lee argumento de comparacion , 0 se realiza comparacion y/o asignacion .
             }
-
-            formatReader = startStringChecker(format,11,"%s;","%c;","%d;","%i;","%ld;","%li;","%lld;","%lli;","%f;","-->","<--");
-            stringCutter(format,2);
+            formatReader = startStringChecker(format2,11,"%s;","%c;","%d;","%i;","%ld;","%li;","%lld;","%lli;","%f;","-->","<--"); // Leo la cadena de fomato.
+            stringCutter(format2,2); //Corto la cadena de formato para poder leer el siguiente.
             switch(formatReader)
             {
             case 1:
             case 2:
                 if(flagConversion && flag2Conversion == 1 )
                 {
-                    strcpy(argumentToCompare,va_arg(arguments,char*));
+                    strcpy(argumentToCompare,va_arg(arguments,char*)); //Leo un argumento que deberia ser un string para comparar con un dato del archivo que debe convertirse.
                 }
                 else if( flagConversion && flag2Conversion == 0 )
                 {
-                    lastFormat = formatReader;
-                    stringPointer = stringConditionalConversor(conversionArgument,argumentToCompare, va_arg(arguments,void*));
+                    lastFormat = formatReader; //Guardo el ultimo formato leido.
+                    stringPointer = stringConditionalConversor(conversionArgument,argumentToCompare, va_arg(arguments,char*)); //Comparo argumento a convertir con string de parametro.
                     if( stringPointer != NULL && !flag3)
                     {
-                        strcpy( ((char*)pointer),stringPointer);
+                        strcpy( ((char*)pointer),stringPointer); //Si no devuelve null el conversor y flag3 es 0 (no se realizo asignacion) asigno el dato convertido.
                         flag3 = 1;
                     }
                 }
@@ -190,7 +202,8 @@ int csvGenericParser( FILE* filePointer, int dataToParse,char format[], int tota
                 if( flagConversion && flag2Conversion == 0 )
                 {
                     lastFormat = formatReader;
-                    integerPointer = stringConditionalConversor(conversionArgument,argumentToCompare, va_arg(arguments,void*));
+                    integerNumber = va_arg(arguments,int);
+                    integerPointer = stringConditionalConversor(conversionArgument,argumentToCompare, &integerNumber );
                     if( integerPointer != NULL && !flag3)
                     {
                         *((int*)pointer) = *integerPointer;
@@ -209,7 +222,8 @@ int csvGenericParser( FILE* filePointer, int dataToParse,char format[], int tota
                 if( flagConversion && flag2Conversion == 0 )
                 {
                     lastFormat = formatReader;
-                    longPointer = stringConditionalConversor(conversionArgument,argumentToCompare, va_arg(arguments,void*));
+                    longNumber = va_arg(arguments,long);
+                    longPointer = stringConditionalConversor(conversionArgument,argumentToCompare, &longNumber );
                     if( longPointer != NULL  && !flag3)
                     {
                         *((long*)pointer) = *longPointer;
@@ -228,7 +242,8 @@ int csvGenericParser( FILE* filePointer, int dataToParse,char format[], int tota
                 if( flagConversion && flag2Conversion == 0  )
                 {
                     lastFormat = formatReader;
-                    longLongPointer = stringConditionalConversor(conversionArgument,argumentToCompare, va_arg(arguments,void*));
+                    longLongNumber = va_arg(arguments,long long);
+                    longLongPointer = stringConditionalConversor(conversionArgument,argumentToCompare, &longLongNumber );
                     if( longLongPointer != NULL && !flag3)
                     {
                         *((long long*)pointer) = *longLongPointer;
@@ -246,7 +261,8 @@ int csvGenericParser( FILE* filePointer, int dataToParse,char format[], int tota
                 if( flagConversion && flag2Conversion == 0  )
                 {
                     lastFormat = formatReader;
-                    doublePointer = stringConditionalConversor(conversionArgument,argumentToCompare, va_arg(arguments,void*));
+                    doubleNumber = va_arg(arguments,double);
+                    doublePointer = stringConditionalConversor(conversionArgument,argumentToCompare, &doubleNumber);
                     if( doublePointer != NULL && !flag3)
                     {
                         *((double*)pointer) = *doublePointer;
@@ -261,13 +277,14 @@ int csvGenericParser( FILE* filePointer, int dataToParse,char format[], int tota
                 }
                 break;
             case 10:
-                pointer = va_arg(arguments,void*);
-                strcpy( conversionArgument,matrizDatos[j]);
-                j++;
-                flagConversion = 1;
+                pointer = va_arg(arguments,void*); //Si es el caso 10 lei una flechita, que indica un puntero que debe ser cargado con el dato del archivo convertido si es posible.
+                strcpy( conversionArgument,matrizDatos[j]); //El dato de esta posicion lo guardo en el string conversionArgument para comparalo.
+                j++; //Cada vez que leo un dato de la matriz incremento el indice de iteraciones relacionado a la matriz.
+                flagConversion = 1; //Indico que lei un argumento que es de comparacion para su analisis.
                 break;
             case 11:
-                if(flag3 == 0)
+                //Si es el caso 11 lei la flechita de final de parametros de conversion.
+                if(flag3 == 0) //Si la bandera es 0, el dato de conversion no se convirtio nunca, le asigno el dato que vino del archivo segun ultimo formato leido.
                 {
                     switch(lastFormat)
                     {
@@ -277,36 +294,34 @@ int csvGenericParser( FILE* filePointer, int dataToParse,char format[], int tota
                         break;
                     case 3:
                     case 4:
-                        *integerPointer = atoi(conversionArgument);
-                        *((int*)pointer) = *integerPointer;
+                        integerNumber = atoi(conversionArgument);
+                        *((int*)pointer) = integerNumber;
                         break;
                     case 5:
                     case 6:
-                        *longPointer = atol(conversionArgument);
-                        *((long*)pointer) = *longPointer;
+                        longNumber = atol(conversionArgument);
+                        *((long*)pointer) = longNumber;
                         break;
                     case 7:
                     case 8:
-                        *longLongPointer = atoll(conversionArgument);
-                        *((long long*)pointer) = *longLongPointer;
+                        longLongNumber = atoll(conversionArgument);
+                        *((long long*)pointer) = longLongNumber;
                         break;
                     case 9:
-                        *doublePointer = atof(conversionArgument);
-                        *((double*)pointer) = *doublePointer;
+                        doubleNumber = atof(conversionArgument);
+                        *((double*)pointer) = doubleNumber;
                         break;
                     }
                 }
-                i--;
-                flag3 = 0;
+                i--; //Como no se lee ningun parametro bajo la variable de iteraciones para evitar colapsos.
+                flag3 = 0; //Resstablezco todas las banderas relacionadas con argumentos de conversion.
                 flagConversion = 0;
                 flag2Conversion = 0;
                 break;
             }
         }
-        strcpy(format,auxFormat);
         va_end(arguments);
     }
-
     return verify;
 }
 
